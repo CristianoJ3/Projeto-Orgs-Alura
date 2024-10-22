81af0cbb4c1264425b86ed34b50b33c1aeaabaf2
CristianoJ3 <cristianocarmo.junior@gmail.com>
2024-10-22 17:38:51 -0300

    Adicionada uma nova Activity para criar uma tela inicial para o App, ainda não está em sua versão final, é apenas um protótipo de tela para melhorar a interface da aplicação.

---
+++ app/src/main/java/com/example/orgs/ui/activity/ActivityInicial.kt
@@ -0,0 +1,29 @@
+package com.example.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.orgs.databinding.ActivityInicialBinding
+
+class ActivityInicial : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInicialBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inflando o layout da tela
+        binding = ActivityInicialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Configurando o clique no botão "Vamos lá"
+        binding.activityInicialButton.setOnClickListener {
+            // Ir para a próxima Activity (ListaProdutosActivity)
+            val intent = Intent(this, ListaProdutosActivity::class.java)
+            startActivity(intent)
+
+            // Finalizar a Activity Inicial para que o usuário não volte para ela
+            finish()
+        }
+    }
+}
